5bad9da7fbec0ec89b06f5dc0de4c363140fc648
Jeffrey Crewe-Brown <jeffrey.crewe-brown@aax.co.za>
2017-09-27 14:10:53 +0200

    included authentication

---
+++ project/tests/test.py
@@ -0,0 +1,12 @@
+# project/tests/test.py
+
+import unittest
+
+
+class TestTestEnv(unittest.TestCase):
+    def test_true(self):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
